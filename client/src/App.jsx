fd652e413d08a93c4346f6debfd488ac9ed5c5cb
Neorace22 <siddarthsajeev2002@gmail.com>
2023-09-21 22:34:21 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle '>
+
+
+      {/* gradients */}
+
+      <div className="gradient-01 z-0 absolute"></div>
+      <div className='gradient-02 z-0 absolute'></div>
+
+      {/* header */}
+      <div className='uppercase font-bold text-2xl text-center mb-3'>ChatGPT2.0</div>
+
+
+      {/* body */}
+      <div>body</div>
+
+      {/* input */}
+      <div>input</div>
+    </div>
+  )
+}
+
+export default App
